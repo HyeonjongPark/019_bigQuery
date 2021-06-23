7df8752acb35069d1577c8dab78d378face00f95
HyeonjongPark <guswhd9844@gmail.com>
2021-06-23 18:57:09 +0900

    test

---
+++ R/src/test_bq.R
@@ -0,0 +1,10 @@
+
+library(bigrquery)
+
+bq_test_project()
+billing <- bq_test_project() # replace this with your project ID 
+sql <- "SELECT year, month, day, weight_pounds FROM `publicdata.samples.natality`"
+
+tb <- bq_project_query(billing, sql)
+bq_table_download(tb, max_results = 10)
+
